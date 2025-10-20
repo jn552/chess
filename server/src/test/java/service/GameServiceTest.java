83d0d1dea821fbf57c18a5b35ec909ecc5a3cca9
Jeremy Ng <168603096+jn552@users.noreply.github.com>
2025-10-20 16:50:07 -0600

    finished GameService unit tests

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -6,11 +6,12 @@ import dataaccess.GameDAOMemory;
 import dataaccess.UserDAOMemory;
 import exception.BadRequestException;
 import exception.NotAuthException;
-import model.AuthData;
-import model.CreateGameData;
-import model.GameData;
-import model.UserData;
+import exception.TakenException;
+import model.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,8 +22,6 @@ class GameServiceTest {
     AuthDAOMemory authDao = new AuthDAOMemory();
 
     // instantiate things to add to DAOs
-    GameData game = new GameData(12, "testwhite", "testblack", "testgame", new ChessGame());
-    UserData user = new UserData("jeremy", "12345", "jeremy@email.com");
     AuthData auth = new AuthData("jeremy", "34-53.6");
 
     // instantiate the game service
@@ -41,20 +40,64 @@ class GameServiceTest {
         authDao.save(auth);
         testGameService.createGame(new CreateGameData(("testGame")), "34-53.6");
         assert(gameDao.find(1) != null);
-
     }
 
     @Test
-    void joinGame() {
-        // test where I try to join as white but white is in there already
+    void joinGameInvalid() {
+        // add game where white player is already in
+        GameData game = new GameData(4, "white", null, "testGame", new ChessGame());
+        authDao.save(auth);
+        gameDao.save(game);
 
-        // test I actually joined a game (I am white, white is empty, so check that white username was updated)
+        // should reject request to join
+        assertThrows(TakenException.class, () -> {
+            testGameService.joinGame(new JoinRequestData(ChessGame.TeamColor.WHITE, 4),"34-53.6");
+        });
     }
 
     @Test
-    void listGames() {
+    void joinGameValid() throws BadRequestException {
+        // add game where white player is already in
+        GameData game = new GameData(4, null, "black", "testGame", new ChessGame());
+        authDao.save(auth);
+        gameDao.save(game);
+
+        // join
+        testGameService.joinGame(new JoinRequestData(ChessGame.TeamColor.WHITE, 4),"34-53.6");
+
+        // checking if white username, which I joined as was updated
+        assert gameDao.find(4).whiteUsername().equals("jeremy");
+    }
+
+    @Test
+    void listGamesNotAuth() {
         // test where list games when no games doesn't throw error
+        assertThrows(NotAuthException.class, () -> {
+            testGameService.listGames("fakeAuth");
+        });
+    }
 
-        // test list games works
+    @Test
+    void listGamesValid() {
+        authDao.save(auth);
+
+        // add a bunch of games
+        GameData game1 = new GameData(4, "white", null, "testGame", new ChessGame());
+        GameData game2 = new GameData(5, "jeremy", "bob", "testGame1", new ChessGame());
+        GameData game3 = new GameData(6, "bob", "harry", "testGame2", new ChessGame());
+        GameData game4 = new GameData(7, null, null, "testGame7", new ChessGame());
+        gameDao.save(game1);
+        gameDao.save(game2);
+        gameDao.save(game3);
+        gameDao.save(game4);
+
+        // getting actual list of games that should list
+        Collection<GameData> realGames = new ArrayList<>();
+        realGames.add(game1);
+        realGames.add(game2);
+        realGames.add(game3);
+        realGames.add(game4);
+
+        assert testGameService.listGames("34-53.6").equals(realGames);
     }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,10 +1,29 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.AuthDAOMemory;
+import dataaccess.GameDAOMemory;
+import dataaccess.UserDAOMemory;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
+
+    // instantiate some DAOs
+    AuthDAOMemory authDao = new AuthDAOMemory();
+    UserDAOMemory userDao = new UserDAOMemory();
+
+    // instantiate things to add to DAOs
+    GameData game = new GameData(12, "testwhite", "testblack", "testgame", new ChessGame());
+    UserData user = new UserData("jeremy", "12345", "jeremy@email.com");
+    AuthData auth = new AuthData("jeremy", "34-53.6");
+
+    // instantiate the clear service
+    UserService testUserService = new UserService(userDao, authDao);
 
     @Test
     void registerUser() {
