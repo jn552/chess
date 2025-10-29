152f6fcb365555ed341afd6f7667fc60d1f69b5e
Jeremy Ng <168603096+jn552@users.noreply.github.com>
2025-10-28 19:49:59 -0600

    mimicked pet shop example (hopefully set up databse correctly); still need to implement save, find and clear

--- server/src/main/java/dataaccess/UserDAOSQL.java
+++ server/src/main/java/dataaccess/UserDAOSQL.java
@@ -1,4 +1,51 @@
 package dataaccess;
 
-public class UserDAOSQL {
+import model.UserData;
+
+import java.sql.*;
+
+public class UserDAOSQL implements UserDAOInterface{
+
+    public UserDAOSQL() throws DataAccessException {
+        configureDatabase();
+    }
+
+    @Override
+    public void save(UserData user){
+
+    }
+
+    @Override
+    public UserData find(String username){
+        return new UserData(null, null, null);
+    }
+
+    @Override
+    public void clear(){
+
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  user (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
 }
