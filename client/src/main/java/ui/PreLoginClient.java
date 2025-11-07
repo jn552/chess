e2a4b0362bce9a1ccb44b8e923188aeae93b19d1
Jeremy Ng <168603096+jn552@users.noreply.github.com>
2025-11-06 17:37:29 -0700

    worked on prelogin client and repl; used code from lecture videos as a guide

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,4 +1,16 @@
 package ui;
 
+import com.sun.nio.sctp.NotificationHandler;
+
 public class PreLoginClient {
+    public final String serverUrl;
+    private final NotificationHandler notificationHandler;
+
+    public PreLoginClient(String serverUrl, NotificationHandler notificationHandler) {
+        this.serverUrl = serverUrl;
+        this.notificationHandler = notificationHandler;
+
+    }
+
+
 }
--- client/src/main/java/ui/PreLoginRepl.java
+++ client/src/main/java/ui/PreLoginRepl.java
@@ -1,4 +1,51 @@
 package ui;
 
-public class PreLoginRepl {
+import com.sun.nio.sctp.HandlerResult;
+import com.sun.nio.sctp.Notification;
+import com.sun.nio.sctp.NotificationHandler;
+
+import java.util.Scanner;
+
+import static java.awt.Color.BLUE;
+
+public class PreLoginRepl implements NotificationHandler {
+    private final PreLoginClient client;
+
+    public PreLoginRepl(String serverUrl) {
+        client = new PreLoginClient(serverUrl, this);
+    }
+    public void run() {
+
+        // opening messages
+        System.out.println("\uD83D\uDC36 Welcome to a CS240 Chess Server. Sign in to start.");
+        System.out.println(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+
+        // main loop
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            }
+
+            catch (Throwable e) {
+                var msg = e.toString();
+                System.out.println(msg);
+            }
+        }
+    }
+
+    private void printPrompt(){
+        System.out.println("PRINT PROMPT HERE");
+    }
+    @Override
+    public HandlerResult handleNotification(Notification notification, Object attachment) {
+        // INTELLIJ SAID TO DO THIS< NOT SURE WHAT TO DO WITH THIS YET
+        return null;
+    }
 }
