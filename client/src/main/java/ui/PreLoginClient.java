300de1597131a7c639103d47d682ae63357007fa
Jeremy Ng <168603096+jn552@users.noreply.github.com>
2025-11-09 12:45:59 -0700

    worked on prelogin client, added an eval loop--need to add methods in eval

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -2,6 +2,8 @@ package ui;
 
 import com.sun.nio.sctp.NotificationHandler;
 
+import java.util.Arrays;
+
 public class PreLoginClient {
     public final String serverUrl;
     private final NotificationHandler notificationHandler;
@@ -9,7 +11,24 @@ public class PreLoginClient {
     public PreLoginClient(String serverUrl, NotificationHandler notificationHandler) {
         this.serverUrl = serverUrl;
         this.notificationHandler = notificationHandler;
+    }
 
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "help" -> help();
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        }
+        catch (ResponseExeption ex) {
+            return ex.getMessage();
+        }
     }
 
 
