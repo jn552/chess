3210f5834141ff374b8317b985c4c9393436cee0
Jeremy Ng <168603096+jn552@users.noreply.github.com>
2025-11-27 09:59:40 -0700

    draft of connection manager done; broadcast takes gameID loops through all sessions as sends messsgae to session with matching gameID

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,29 +1,29 @@
 package websocket;
 
 import org.eclipse.jetty.websocket.api.Session;
-import webSocket.Notification;
+import websocket.messages.ServerMessage;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
+    public final ConcurrentHashMap<Session, Integer> connections = new ConcurrentHashMap<>();
+
+    public void add(Session session, Integer gameID) {
+        connections.put(session, gameID);
     }
 
     public void remove(Session session) {
         connections.remove(session);
     }
 
-    public void broadcast(Session excludeSession, Notification notification) throws IOException {
+    public void broadcast(int gameID, ServerMessage notification) throws IOException {
         String msg = notification.toString();
-        for (Session c : connections.values()) {
-            if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
-                }
+        for (Map.Entry<Session, Integer> pair : connections.entrySet()) {
+            if (pair.getKey().isOpen() && pair.getValue().equals(gameID)) {
+                pair.getKey().getRemote().sendString(msg);
             }
         }
     }
